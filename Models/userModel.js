643bb6a975c0fb335c01fb2bc9da0b68ea6f20c8
ShalikaAshan01 <shalikaashan01@gmail.com>
2019-05-27 08:50:59 +0530

    sign in api added

---
+++ Models/userModel.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    _token: {
+        type: Schema.Types.Mixed
+    },
+    telephoneNumber: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    paymentInfo: {
+        type: Schema.Types.Mixed
+    },
+    status: {
+        type: String,
+        default: "activated"
+    },
+    role: {
+        type: String,
+        default: "user"
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    updatedAt: {
+        type: Date
+    }
+});
+var user = mongoose.model("user", userSchema);
+module.exports = user;
