c0e9441440595cb92944cb83f5fd6d06abd7884e
Pasan Godamune <pasanjg@gmail.com>
2019-06-22 11:56:18 +0530

    student-course endpoint changes

--- models/StudentCourse.js
+++ models/StudentCourse.js
@@ -4,6 +4,7 @@ const studentCourseSchema = mongoose.Schema({
     id: mongoose.Schema.Types.ObjectId,
     studentId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     courseId: { type: mongoose.Schema.Types.ObjectId, ref: 'course', required: true },
+    courseName: { type: String, required: true },
     created_at: { type: Date, required: true, default: Date.now() },
     updated_at: { type: Date, required: true, default: Date.now() }
 });
